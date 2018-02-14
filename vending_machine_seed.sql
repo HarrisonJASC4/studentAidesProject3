2a84ca0a49a3158cdbb218a22a5615b9fd8da7c8
HarrisonJASC4 <30082155+HarrisonJASC4@users.noreply.github.com>
2018-02-13 19:15:20 -0500

    Add files via upload

--- vending_machine_seed.sql
+++ vending_machine_seed.sql
@@ -23,3 +23,27 @@ VALUES
     ("chips", 9, 'snack', 1.30),
     ("pop tarts", 7, 'snack', 2.45),
     ("trail mix", 5, 'snack', 5.00);
+
+--     Show all of the rows of data
+-- Show only rows that have a type of “candy”
+-- Show all rows in descending order by name
+-- Show only rows that have a type of “liquid” AND have an amount under 5
+-- Show only rows that have a type of “snack” in ascending order by cost
+-- BONUS: Create a new table named “snacks” with the result of the last command!
+-- Show the name and quantity of the least expensive item
+-- Show the count of different items (basically the total number of rows)
+-- NOTE: this requires using aggregate functions
+-- Show the average cost of an item as “average cost”
+-- NOTE: this requires using an alias for the result
+-- Show the name and total cost of each item. The total cost = quantity of the item * cost
+-- Show all items grouped by type
+-- NOTE: this requires using the “GROUP BY” clause
+-- BONUS: Show the total cost of all items in the vending machine (sum of (each item’s cost * quantity of the item)) as “total cost”
+
+
+    SELECT * FROM vending_machine
+    SELECT * FROM vending_machine where type = 'candy'
+    SELECT * FROM vending_machine where type = 'liquid' , amount < 5
+    SELECT * FROM vending_machine ORDER BY name DESC 
+    SELECT * FROM vending_machine where type = 'snack' ORDER By cost ASC
+    SELECT * FROM vending_machine where name, cost * amount 
